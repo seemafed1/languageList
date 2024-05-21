50e7e8fa8d7f59cfd7642473240dae97a5bb9d68
seemafed1 <fed1@horecah.com>
2024-05-21 12:05:40 +0530

    update  popup

--- src/stories/TabButton.stories.ts
+++ src/stories/TabButton.stories.ts
@@ -19,7 +19,7 @@ type Story = StoryObj<typeof meta>;
 export const Primary: Story = {
   args: {
     // primary: true,
-    tabArray: [ {label : 'Tab 1'}, {label : 'Tab 2'}, {label : 'Tab 3'} ]
+    tabArray: [ {label : 'Tab 1'}, {label : 'Tab 2'}, {label : 'Tab 3'}, {label : 'Tab 3'}, {label : 'Tab 3'}, {label : 'Tab 3'} ]
   },
 };
 
--- src/stories/TabButton.tsx
+++ src/stories/TabButton.tsx
@@ -2,12 +2,12 @@ import { Tab, Tabs } from '@mui/material';
 import React, { useState } from 'react';
 
 type tabArray = {
-  label: string
+  label: string,
 }
 type Props = {
   tabArray: tabArray[]
 }
-const TabButton = ({ tabArray}: Props) => {
+const TabButton = ({ tabArray }: Props) => {
   const [selectedTab, setSelectedTab] = useState(0);
 
   const handleTabChange = (event: React.SyntheticEvent, newValue: number) => {
@@ -36,11 +36,27 @@ const TabButton = ({ tabArray}: Props) => {
                 backgroundColor: '#FFF1E6',
                 borderBottom: '2px solid #FE8B4C'
               },
-            }}/>
+            }} />
           ))
         }
-          
+
       </Tabs>
+
+      {/* <Tabs
+        value={''}
+        onChange={() => ''}
+        variant="scrollable"
+        scrollButtons="auto"
+        aria-label="scrollable auto tabs example"
+      >
+        <Tab label="Item One" />
+        <Tab label="Item Two" />
+        <Tab label="Item Three" />
+        <Tab label="Item Four" />
+        <Tab label="Item Five" />
+        <Tab label="Item Six" />
+        <Tab label="Item Seven" />
+      </Tabs> */}
     </div>
   );
 }
